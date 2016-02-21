7f3eae0c302798577e40502e3bf76455e6694738
profh <profh@cmu.edu>
2016-02-20 22:43:34 -0500

    Starter code for phase 3 of the Creamery project

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Routes for main resources go here
+
+
+  # Semi-static page routes go here
+
+  
+  # Set the root url here
+
+  
+end
---
+++ db/migrate/20160129152216_create_stores.rb
@@ -0,0 +1,17 @@
+class CreateStores < ActiveRecord::Migration
+  def change
+    create_table :stores do |t|
+      t.string :name
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :phone
+      t.float :latitude
+      t.float :longitude
+      t.boolean :active, default: true
+
+      # t.timestamps null: false
+    end
+  end
+end
